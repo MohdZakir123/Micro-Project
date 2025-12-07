4f5afa6e88b30a37317a032b56d4dc15b56a1f9c
MohdZakir123 <109594593+MohdZakir123@users.noreply.github.com>
2025-12-07 12:45:16 +0530

    Refactor bill calculator and fix discount logic
    Refactored bill calculator logic and fixed discount calculation.

--- micro project.py
+++ micro project.py
@@ -1,20 +1,4 @@
-# price = int(input("Enter the price of the item: "))
-# quality = int(input("Enter the quantity of the item: "))
-# tax = 1.00 * price * quantity
-# price += tax
-# if price > 1000:
-#     discount = price * 0.10
-#     final_price = price - discount
-#     totalbill = final_price * quality
-#     print(int("Final price to be paid: ", totalbill))
-#     print(f"Discount applied: {discount}")
-# else:
-#     final_price = price
-#     print("No discount applied.")
-#     print(f"Final price to be paid: {final_price * quality:.1f}")
-# errors not fixed trying new againnnnnnnnnn
-    
-    
+#Bill Calculator Program
 price = float(input("Enter the Price of the item: "))
     
 quantity = int(input("Enter the Quantity of the item: "))
@@ -34,4 +18,5 @@ if price > 1000:
     else:
         final_price = price
         print("No discount applied.")
+
         print(f"Final price to be paid: {final_price * quantity:.1f}")
